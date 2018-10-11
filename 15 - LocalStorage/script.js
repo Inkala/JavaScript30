9a0d2f3886e5bf1f5d625a57680d7d9852026ef6
Marce Ramirez <marceram87@gmail.com>
2018-10-11 18:09:47 +0200

    Done

--- 15 - LocalStorage/script.js
+++ 15 - LocalStorage/script.js
@@ -1,6 +1,15 @@
 const addItems = document.querySelector('.add-items');
+const deleteItem = document.querySelector('.delete-button');
 const itemsList = document.querySelector('.plates');
-const items = JSON.parse(localStorage.getItem('items')) || [];
+const checkAll = document.querySelector('.button-check')
+const clearAll = document.querySelector('.button-clear ')
+let items = JSON.parse(localStorage.getItem('items')) || [];
+
+// if (items.length === 0) {
+//   console.log(items)
+//   const buttons = document.querySelectorAll('.button-all');
+//   buttons.forEach(button => button.disabled = true);
+// }
 
 function addItem(e) {
   e.preventDefault();
@@ -22,23 +31,46 @@ function populateList(plates = [], platesList) {
       <li>
         <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
         <label for="item${i}">${plate.text}</label>
+        <input type="button" class="delete-button" value="x" onClick="deleteElement(${i})">
       </li>
     `;
   }).join('');
 }
 
 function toggleDone(e) {
-  if(!e.target.matches('input')) return;
-  const el = e.target;
-  const index = el.dataset.index;
+  if (e.target.type !== 'checkbox') return;
+  const index = e.target.dataset.index;
   items[index].done = !items[index].done;
   localStorage.setItem('items', JSON.stringify(items));
   populateList(items, itemsList);
 }
 
+function deleteElement(i) {
+  items.splice(i, 1);
+  localStorage.setItem('items', JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
+function handleChecks() {
+  console.log('handleChecks clicked!');
+}
+
+function hancleClear() {
+  if (confirm('Are you sure?')) {
+    items = [];
+    itemsList.innerHTML = '';
+    populateList(items, itemsList);
+    localStorage.clear();
+  }
+}
+
 addItems.addEventListener('submit', addItem);
 itemsList.addEventListener('click', toggleDone);
+checkAll.addEventListener('click', handleChecks);
+clearAll.addEventListener('click', hancleClear);
+
+
 populateList(items, itemsList);
 
-// Create a delete button per element
-// Create a button to check, uncheck or delete all
+// Create a button to check and uncheck all
+// Disable new buttons when list is empty
