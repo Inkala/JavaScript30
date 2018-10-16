458b4194003706c456058e2c7e91af5998b87d75
Marce Ramirez <marceram87@gmail.com>
2018-10-16 18:21:57 +0200

    17 - Sort Without Articles done

---
+++ 17 - Sort Without Articles/script.js
@@ -0,0 +1,49 @@
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+const strip = bandName => bandName
+  .replace(/^(a |an |the )/i, '').trim();
+
+const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+
+document.querySelector('#bands').innerHTML = sortedBands
+  .map(band => `<li>${band}</li>`)
+  .join('');
+
+
+// const sortedBands = bands.map(band => {
+//   let bandArr = band.split(' ');
+//   if (bandArr[0] === 'A' || bandArr[0] === 'An' || bandArr[0] === 'The') {
+//     bandArr.push(bandArr.shift());
+//   }
+//   return bandArr.join(' ');
+// }).sort();
+
+// const fixBandsNames = sortedBands.map(band => {
+//   let bandArr = band.split(' ');
+//   const len = bandArr.length - 1;
+//   if (bandArr[len] === 'A' || bandArr[len] === 'An' || bandArr[len] === 'The') {
+//     bandArr.unshift(bandArr.pop());
+//   }
+//   return bandArr.join(' ');
+// })
+
+// document.querySelector('#bands').innerHTML = fixBandsNames.map(band => {
+//   return `<li>${band}</li>`;
+// }).join('');
+
+
+/*
+- A Skylit Drive
+- An Old Dog
+- Anywhere But Here
+- Counterparts
+- Norma Jean
+- Oh, Sleeper
+- Pierce the Veil
+- Say Anything
+- The Bled
+- The Devil Wears Prada
+- The Midway State
+- The Plot in You
+- We Came as Romans
+*/
