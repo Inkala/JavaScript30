0d8d3df0cf35754b307e9b0aca2c1f097cf9154f
Marce Ramirez <marceram87@gmail.com>
2018-10-17 18:00:52 +0200

    19 - Webcam Fun at 17:30

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,45 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    }).catch(err => {
+      console.error(`OH NO!!!`, err);
+    });
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 16)
+}
+
+function takePhoto() {
+  // Play sound
+  snap.cirrentTime = 0;
+  snap.play();
+
+  // get image data
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'ScreenShot');
+  link.textContent = 'Download Image';
+  strip.insertBefore(link, strip.firstChild)
+  console.log(data)
+}
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
+
+// HTMLMediaElement.srcObject
